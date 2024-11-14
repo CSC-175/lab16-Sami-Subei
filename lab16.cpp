ca1d6294feae832fe61bc4cebb0e03a601cbdeb4
Sami-Subei <ssubei@umich.edu>
2024-11-14 11:33:08 -0500

    Update lab16.cpp

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,11 @@
-// Code to implement the gcf function goes here
+#include <iostream>
+using namespace std;
+
+int gcf(int a, int b) {
+
+    for (int i = b<=a ? b:a ; i > 1; i--) {
+        if (a%i==0 && b%i==0) return i;
+    }
+
+    return 1;
+}
